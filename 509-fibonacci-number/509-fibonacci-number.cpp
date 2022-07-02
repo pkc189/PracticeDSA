a914c21dda21477d37321437035234f7cf625bae
Pankaj Ku Chouhan <43536789+pkc189@users.noreply.github.com>
2022-07-02 15:19:23 +0530

    Time: 0 ms (100.00%), Space: 6 MB (41.46%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,23 +1,21 @@
 class Solution {
-public:
+  public:
     int fib(int n) {
-        
-        if(n==0 || n==1)
-            return n;
-        
-        
-        int prevOne=0;
-        int prevTwo=1;
-        int curr=0;
-        
-        for(int i=1;i<n;i++)
-        {
-             curr=prevOne+prevTwo;
-            prevOne=prevTwo;
-            prevTwo=curr;
-            
-        }
-        
-       return curr; 
+
+      if (n == 0 || n == 1)
+        return n;
+
+      int prevOne = 0;
+      int prevTwo = 1;
+      int curr = 0;
+
+      for (int i = 1; i < n; i++) {
+        curr = prevOne + prevTwo;
+        prevOne = prevTwo;
+        prevTwo = curr;
+
+      }
+
+      return curr;
     }
 };
