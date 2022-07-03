3304b4d6877a69920cee3dc1e8ee72897ecef400
Pankaj Ku Chouhan <43536789+pkc189@users.noreply.github.com>
2022-07-03 22:52:06 +0530

    Time: 129 ms (96.16%), Space: 64.7 MB (18.75%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.cpp
+++ 938-range-sum-of-bst/938-range-sum-of-bst.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
-    int dfs(TreeNode* node, int sum, int low, int high){
-        if (node == nullptr) return sum;
-        else if(node->val >= low && node->val <= high){
-            sum += node->val;        
-            sum = dfs(node->left, sum, low, high);
-            sum = dfs(node->right, sum, low, high);}
-        else if(node->val < low) sum = dfs(node->right, sum, low, high);
-        else if(node->val > high) sum = dfs(node->left, sum, low, high);
-        return sum;
-    }
     int rangeSumBST(TreeNode* root, int low, int high) {
-        int sum = 0;
-        sum = dfs(root, sum, low, high);
+        int sum=0;
+        if(!root)return 0;
+        if(root->val>=low && root->val<=high)sum+=root->val;
+        sum+=rangeSumBST(root->left,low,high);
+        sum+=rangeSumBST(root->right,low,high);
         return sum;
     }
 };
