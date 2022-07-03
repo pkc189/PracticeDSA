ba502e340e317da6c5169f059433f97169011343
Pankaj Ku Chouhan <43536789+pkc189@users.noreply.github.com>
2022-07-03 22:50:26 +0530

    Time: 210 ms (37.63%), Space: 64.7 MB (60.96%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.cpp
+++ 938-range-sum-of-bst/938-range-sum-of-bst.cpp
@@ -1,43 +1,18 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    
-    int rangeSumBST(TreeNode* root, int low, int high) {
-        
-        int sum=0;
-        
-         traverse(root,low,high,sum);
-        
+    int dfs(TreeNode* node, int sum, int low, int high){
+        if (node == nullptr) return sum;
+        else if(node->val >= low && node->val <= high){
+            sum += node->val;        
+            sum = dfs(node->left, sum, low, high);
+            sum = dfs(node->right, sum, low, high);}
+        else if(node->val < low) sum = dfs(node->right, sum, low, high);
+        else if(node->val > high) sum = dfs(node->left, sum, low, high);
         return sum;
     }
-    
-   void traverse(TreeNode* root, int low, int high,int &sum)
-    {
-       
-       if(root==NULL)
-           return;
-       
-       if(root->val>=low && root->val<=high)
-       { sum+=root->val;
-      }
-       
-       
-       
-       
-       traverse(root->left,low,high,sum);
-       traverse(root->right,low,high,sum);
-       
-   
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        int sum = 0;
+        sum = dfs(root, sum, low, high);
+        return sum;
     }
-    
 };
