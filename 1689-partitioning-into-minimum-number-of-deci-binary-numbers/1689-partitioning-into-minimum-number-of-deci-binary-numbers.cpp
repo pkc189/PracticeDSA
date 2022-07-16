d54e913820f51521831d7a3fbbc9d6c02a2045ee
Pankaj Chouhan <43536789+pkc189@users.noreply.github.com>
2022-07-16 13:44:10 +0530

    Time: 41 ms (61.04%), Space: 13.4 MB (91.60%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int minPartitions(string n) {
-        
-        ios_base::sync_with_stdio(0);
-        cin.tie(0); cout.tie(0);
-        
-        int ans = 0;
+                
+        int answer = 0;
         for(int i = 9; i >= 0; i--){
             char c = i+'0';
-            //size_t found = n.find(c);
+            
             if( n.find(c) != string::npos )
                 return i;
         }
-        return ans;
+        return answer;
     }
 };
