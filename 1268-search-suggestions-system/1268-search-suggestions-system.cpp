8050bbb653ee8d7b4c0c631ab5a769e07fff2fb6
Pankaj Chouhan <43536789+pkc189@users.noreply.github.com>
2022-07-17 23:17:06 +0530

    Time: 352 ms (23.51%), Space: 96.4 MB (25.80%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -0,0 +1,69 @@
+
+class TrieNode{
+    public:
+    unordered_map<char,TrieNode*>leaves;
+    vector<string>suggestion;
+};
+
+
+
+void insert(TrieNode*root,string word)
+{
+    for(auto a:word)
+    {
+        if(root->leaves.find(a)==root->leaves.end())
+        {
+            root->leaves[a]= new TrieNode();
+        }
+            root=root->leaves[a];
+        
+        
+        if(root->suggestion.size()<3)
+            root->suggestion.push_back(word);
+        
+    }
+}
+
+
+
+
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+     
+        
+         vector<vector<string>> result(searchWord.size());
+        
+        
+        sort(products.begin(),products.end());
+        
+        TrieNode* root = new TrieNode();
+        
+        for(auto a:products)
+            insert(root,a);
+        
+        int i=0;
+        for(auto a:searchWord)
+        {
+            auto it = root->leaves.find(a);
+            
+            if(it !=root->leaves.end() )
+            {
+                root=it->second;
+            }
+            else
+                break;
+            result[i]=root->suggestion;
+            i++;
+        }
+        
+        
+        
+        
+        
+        return result;
+        
+        
+        
+    }
+};
