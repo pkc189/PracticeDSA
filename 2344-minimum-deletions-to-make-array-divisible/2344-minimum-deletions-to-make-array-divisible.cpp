d9011e8ba90ac38622c48f37aee1f902bff1b65a
Pankaj Chouhan <43536789+pkc189@users.noreply.github.com>
2022-07-17 11:45:14 +0530

    Time: 261 ms (50.00%), Space: 77.9 MB (25.00%) - LeetHub

--- 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
+++ 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
@@ -5,7 +5,7 @@ public:
         int g=numsDivide[0];
         
         for(auto a:numsDivide)
-            g=gcd(g,a);
+            g = __gcd(g, a);
         
         sort(nums.begin(),nums.end());
    
@@ -28,4 +28,23 @@ public:
         
         
     }
+    
+    
+     int GCD(int x, int y) {
+        int ans = 1;
+        
+        for(long i = 1; i * i <= x; i++) {
+            if(x % i)
+                continue;
+            if(y % i == 0) {
+                ans = max(ans, (int)i);
+            } 
+            if(y % (x / i) == 0) {
+                ans = max(ans, (int)(x / i));
+            }
+        }
+        
+        return ans;
+    }
+    
 };
