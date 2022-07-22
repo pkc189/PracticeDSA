e5dbbbe7d8f7e66fe31468d7892169db9d161e9a
Pankaj Chouhan <43536789+pkc189@users.noreply.github.com>
2022-07-23 00:36:55 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (24.52%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int countOdds(int low, int high) {
    
-  
+  ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
         
          return (high + 1) / 2 - low / 2;
         
