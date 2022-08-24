516c11d6022a39296d124de901d93faff55d4331
Pankaj Chouhan <43536789+pkc189@users.noreply.github.com>
2022-08-24 07:53:53 +0530

    Time: 2 ms (90.35%), Space: 6.3 MB (94.82%) - LeetHub

--- 1147-longest-chunked-palindrome-decomposition/1147-longest-chunked-palindrome-decomposition.cpp
+++ 1147-longest-chunked-palindrome-decomposition/1147-longest-chunked-palindrome-decomposition.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     int longestDecomposition(string text) {
-        int count=0;
-        int n = text.size();
-        string l="",r="";
-        for(int i=0;i<text.size();i++)
-        {
-            l = l + text[i];
-            r = text[n-1-i] + r;
-            
-            if(l==r)
-               { count++;l="";r="";}
-            
+        long long lh = 0LL, rh = 0LL, powers=1LL, MOD = 1000000007LL;
+        int l =0, r = text.size()-1;
+        int ans =0;
+        while(l<r){
+            lh = (lh*26LL + text[l++]-'a'+1) % MOD;
+            rh = (rh + (text[r--]-'a'+1)*powers% MOD )% MOD;
+            if(lh == rh){
+                ans +=2;
+                powers = 1;
+                lh = rh =0LL;
+            }
+            else
+                powers = (powers*26LL) % MOD;
         }
-        
-       return count; 
+        if(l==r || powers > 1)
+            return ans+1;
+        return ans;
     }
 };
