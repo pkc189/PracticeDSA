8be25478d7ca1cfa5e554104c7e9a822cf64edd0
Pankaj Chouhan <43536789+pkc189@users.noreply.github.com>
2022-09-18 23:39:03 +0530

    Time: 354 ms (7.58%), Space: 114.5 MB (5.46%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,43 @@
+class Solution
+{
+    public:
+
+        vector<vector < int>> ans;
+
+    void solve(int i, vector<int> temp, vector<int> c, int t)
+    {
+        if (t == 0)
+        {
+            ans.push_back(temp);
+            return;
+        }
+        
+        
+        if(t<0)
+            return;
+        if(i==c.size())
+            return;
+        
+        
+        
+        solve(i+1,temp,c,t);
+        
+        temp.push_back(c[i]);
+        solve(i,temp,c,t-c[i]);
+        
+      //  temp.pop_back();
+        
+        
+        
+    }
+
+    vector<vector < int>> combinationSum(vector<int> &c, int t)
+    {
+
+        vector<int> temp;
+
+        solve(0, temp, c, t);
+
+        return ans;
+    }
+};
