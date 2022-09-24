626e061a3fb2cb104f827b766af7815093056245
Pankaj Chouhan <43536789+pkc189@users.noreply.github.com>
2022-09-24 13:20:08 +0530

    Time: 411 ms (60.74%), Space: 80.8 MB (91.99%) - LeetHub

--- 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.cpp
+++ 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.cpp
@@ -1,38 +1,25 @@
-class Solution {
-public:
-    int halveArray(vector<int>& nums) {
-    
-        
-        double sum = accumulate(nums.begin(),nums.end(),0.0);
-        priority_queue<double>pq(nums.begin(),nums.end());
-   
-        
-        double tot=0;
-        int count=0;
-        while((sum-tot)>sum/2)
+class Solution
+{
+    public:
+        int halveArray(vector<int> &nums)
         {
-//                    cout<<sum-tot<<" "<<sum<<endl;
-             double a = pq.top()/2;
+
+            double sum = accumulate(nums.begin(), nums.end(), 0.0);
+            priority_queue<double> pq(nums.begin(), nums.end());
+
+            double tot = 0;
+            int count = 0;
+            while ((sum - tot) > sum / 2)
+            {
+
+                double a = pq.top() / 2;
                 pq.pop();
-             tot+=a;
-              
-         
-       
+                tot += a;
+
                 pq.push(a);
-             count++;
-            
-            
-       // double x = pq.top();
-       // pq.pop();
-       //  tot += x / 2;
-       //  pq.push(x / 2);     
-       //  count++;
-            
-     
-            
+                count++;
+            }
+
+            return count;
         }
-        
-        
-        return count;
-    }
 };
