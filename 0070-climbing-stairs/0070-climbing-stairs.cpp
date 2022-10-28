2096d7c4c48f35bd2eb4048352f82a7c794496ba
Pankaj Chouhan <43536789+pkc189@users.noreply.github.com>
2022-10-28 14:22:27 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (83.14%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==0 || n==1)
+            return 1;
+        
+        int prev=1,prev2=1,curr=0;
+        
+        for(int i=2;i<=n;i++)
+        {
+            curr=prev+prev2;
+            prev2=prev;
+            prev=curr;
+        }
+        return curr;
+    }
+};
