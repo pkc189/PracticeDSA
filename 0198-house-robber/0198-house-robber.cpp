28db34509b57c86781d4ee2727639055296eb32b
Pankaj Chouhan <43536789+pkc189@users.noreply.github.com>
2022-10-29 14:17:01 +0530

    Time: 4 ms (41.92%), Space: 7.9 MB (25.63%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,25 +1,23 @@
-class Solution {
-public:
-    
-    int jump(int ind,vector<int>& nums,vector<int>&dp)
+class Solution
+{
+    public:
+
+
+    int rob(vector<int> &nums)
     {
-        if(ind<0)
-            return 0;
+        int n = nums.size();
+        vector<int> dp(nums.size() + 1, -1);
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++)
+        {
+            int pick = nums[i];
+            if(i>1)
+                pick+=dp[i-2];
+            int notPick =dp[i-1];
+            
+            dp[i] = max(pick,notPick);
+        }
         
-        if(dp[ind]!=-1)
-            return dp[ind];
-        
-        int pick = nums[ind] + jump(ind-2,nums,dp);
-        int notPick = 0 + jump(ind-1,nums,dp);
-        
-        return dp[ind] =  max(pick,notPick);
-    }
-    
-    
-    
-    
-    int rob(vector<int>& nums) {
-      vector<int>dp(nums.size()+1,-1);
-       return jump(nums.size()-1,nums,dp);
+        return dp[n-1];
     }
 };
